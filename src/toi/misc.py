bd54d1e3cba74f3548a6f21dc0e01b7f2e077d10
Michail Pevnev <mpevnev@gmail.com>
2018-03-17 21:04:53 +0300

    Add 'pretty_name' function

--- src/toi/misc.py
+++ src/toi/misc.py
@@ -8,3 +8,8 @@ Provides functions that don't fit anywhere else.
 def normalize(string):
     """ Normalize a string. """
     return "".join(string.lower().split())
+
+
+def pretty_name(string):
+    """ Make a name pretty: strip the whitespace and capitalize each word. """
+    return " ".join(map(str.capitalize, string.split()))
