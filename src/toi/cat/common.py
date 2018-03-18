c7a8726c02efa621790d87616a1e18ffe623de5c
Michail Pevnev <mpevnev@gmail.com>
2018-03-18 11:43:36 +0300

    Add a common string for list formatting

--- src/toi/cat/common.py
+++ src/toi/cat/common.py
@@ -5,7 +5,10 @@ Common strings constants.
 AMBIG_CMD = "ambiguous cmd"
 EMPTY_CMD = "empty cmd"
 FAREWELL = "farewell"
+FAREWELL_WITH_PARTY = "farewell with party name"
+OKAY = "okay"
 WHAT = "what"
+LIST_LEADER = "list leader"
 
 CMD_HELP = "help"
 CMD_QUIT = "quit"
